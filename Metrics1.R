ceeb3925d2a25d36debdd046940f4223e487d451
flatfoot56 <flatfoot56@bk.ru>
2019-01-13 19:00:35 +0300

    Metrics1

--- Metrics1.R
+++ Metrics1.R
@@ -9,22 +9,40 @@ female_mean <- mean(df$female)
 female_sd <- sd(df$female)
 
 #Problem 6
-#1
+#a
 fit <- lm(ahe ~ yrseduc, df)
 summary(fit)
 
-#2
+#b
 library(ggplot2)
 test <- function(x) {1.32186 * x -2.60905}
 
 ggplot(df, aes(yrseduc, ahe)) + geom_point() + geom_smooth(method='lm', se = F)
 
-#3
+#c
 library(sandwich)
 library(lmtest)
 coeftest(fit, vcov = vcovHC(fit, type = "HC1"))
 
-#4
+#d
 confint(fit, vcov = vcovHC(fit, type = "HC1"))
 
+#f
+cor(df$ahe, df$yrseduc)
 
+#g
+sqrt(mean(fit$residuals^2))
+
+#Problem 7
+fit2 <- lm(ahe ~ female, df)
+summary(fit2)
+
+#b
+coeftest(fit2, vcov = vcovHC(fit2, type = "HC1"))
+
+#c
+male_mean <- mean(df$ahe[df$female == 0])
+female_mean <- mean(df$ahe[df$female == 1])
+sd_w = sd(df$ahe[df$female == 1])
+sd_m = sd(df$ahe[df$female == 0])
+t.test(df$ahe[df$female == 1], df$ahe[df$female == 0])
