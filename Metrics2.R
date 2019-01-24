2160fe6b9e3ad1895ccf5461f1028e56aea63e95
flatfoot56 <flatfoot56@bk.ru>
2019-01-24 20:51:31 +0300

    metrics

---
+++ Metrics2.R
@@ -0,0 +1,20 @@
+df <- read.csv('cps99_ps1.csv')
+
+#Problem 5
+cor(df$yrseduc, df$female)
+
+#Problem 7
+#a
+fit1 <- lm(df$ahe ~ df$yrseduc, data = df)
+fit2 <- lm(df$ahe ~ df$yrseduc + df$female, data = df)
+
+summary(fit2)
+
+#b
+library(lmtest)
+coeftest(fit2, vcov = vcovHC(fit2, type = "HC1"))
+
+#c
+summary(fit1)
+
+#Problem 8
